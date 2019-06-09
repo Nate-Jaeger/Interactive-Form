5ae17f2cae02db4c33aaa1ec5b2676d7bb889422
Nate Jaeger <Nate.RJS21@gmail.com>
2019-06-08 22:56:47 -0500

    Job Role input feild added
    Working on gettign ti to work when the other option is selected

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const nameInput = document.getElementById("name");
+const $otherTitle = $("#other-title");
 
 
 
 //Set focus on first text feild
 nameInput.focus();
+
+//Hide the 'other-title' input until the 'other' option is selected
+$otherTitle.hide();
+if($('#title').children("option:selected").val() === "other"){
+    $otherTitle.show();
+}
